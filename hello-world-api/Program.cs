be12106ddd715ddc2e15a9921bbf3f9f351e2821
0509pooja <149455505+0509pooja@users.noreply.github.com>
2024-11-09 20:23:53 +0530

    Update Program.cs

--- hello-world-api/Program.cs
+++ hello-world-api/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,6 +20,7 @@ namespace hello_world_api
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000") // Bind to all network interfaces on port 5000
                 .Build();
     }
 }
